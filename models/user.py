5d4b5594cb6291ffeeeee91764f76edae399d099
Battsetseg20 <tsetseg.yo@gmail.com>
2021-03-31 06:58:09 +0000

    add task 7

--- models/user.py
+++ models/user.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+
     __tablename__ = 'users'
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
