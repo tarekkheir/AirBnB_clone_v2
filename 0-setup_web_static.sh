72e059204a00c42c7cc34139b658929c3a768d53
Tarek <kheirtarek@hotmail.fr>
2021-04-15 16:37:25 +0200

    fix 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
-sudo apt-get update && apt-get upgrade
+sudo apt-get update
 sudo apt-get install -y nginx
 
 sudo mkdir -p /data/web_static/shared/
@@ -9,16 +9,14 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 sudo chmod 666 /data/web_static/releases/test/index.html
 
-content="
+echo "
 <html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>"
-
-echo "$content" >> /data/web_static/releases/test/index.html
+</html>" >> /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown -R ubuntu:ubuntu /data/
