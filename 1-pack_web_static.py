c2c564512fc475812e997bdb389b4fdf4625f7cf
Tarek <kheirtarek@hotmail.fr>
2021-04-18 13:59:07 +0200

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,18 +4,18 @@ the web_static folder of your AirBnB Clone repo, using the function do_pack"""
 
 
 from datetime import datetime
-from fabric.api import local, sudo
+from fabric.api import local
 
 
 def do_pack():
     """ make a .tgz files pack"""
-    sudo("mkdir -p versions")
+    local("mkdir -p versions")
     now = datetime.now()
-    archive_path = "web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month,
+    archive_path = "versions/web_static_{}{}{}{}{}{}.tgz".format(now.year, now.month,
                                                         now.day, now.hour,
                                                         now.minute, now.second)
 
-    if local("tar -czvf versions/{} web_static"
+    if local("tar -czvf {} web_static"
              .format(archive_path)).succeeded:
         return archive_path
     else:
