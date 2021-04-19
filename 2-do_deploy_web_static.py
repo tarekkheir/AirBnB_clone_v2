90c20c6b949bfd65623019b6121255b79a32de73
Tarek <kheirtarek@hotmail.fr>
2021-04-19 20:39:11 +0200

    fix definitly task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,17 +15,18 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        sudo("mkdir -p /data/web_static/releases/")
         name_split = archive_path.split('/')
         name_split = name_split[1].split('.')
         name_split = name_split[0]
         filename = archive_path.split('/')
         filename = filename[1]
+        sudo("mkdir -p /data/web_static/releases/{}/".format(name_split))
 
         data = "/data/web_static/releases/"
-        sudo("tar -zxvf /tmp/{} -C {}{}".format(filename, data, name_split))
+        sudo("tar -zxvf /tmp/{} -C {}{}/".format(filename, data, name_split))
         sudo("rm -Rf /tmp/{}".format(filename))
-        sudo("mv {}{}/web_static/* {}releases/{}".format(data, name_split, data, name_split))
+        sudo("mv {}{}/web_static/* {}{}/"
+             .format(data, name_split, data, name_split))
         sudo("rm -rf {}{}/web_static".format(data, name_split))
         sudo("rm -Rf /data/web_static/current")
         sudo("ln -s {}{} /data/web_static/current".format(data, name_split))
