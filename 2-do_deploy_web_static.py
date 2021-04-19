4ff9addb74b56faea0e4ad34fc71d50ef4587f7e
Tarek <kheirtarek@hotmail.fr>
2021-04-19 17:30:06 +0200

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,7 +10,7 @@ env.hosts = ['35.243.232.238', '35.237.104.223']
 
 def do_deploy(archive_path):
     """ do_deploy function"""
-    if os.path.isfile(archive_path) is None:
+    if not os.path.isfile(archive_path):
         return False
 
     try:
@@ -22,17 +22,14 @@ def do_deploy(archive_path):
         filename = archive_path.split('/')
         filename = filename[1]
 
-        local("tar -zxvf /tmp/{} -C /data/web_static/releases/{}"
-              .format(filename, name_split))
-        sudo("rm -Rf /tmp/".format(filename))
         data = "/data/web_static/releases/"
+        local("tar -zxvf /tmp/{} -C {}{}".format(filename, data, name_split))
+        sudo("rm -Rf /tmp/".format(filename))
         sudo("mv {}{}/web_static/* {}releases/{}"
              .format(data, name_split, data, name_split))
-        sudo("rm -rf /data/web_static/releases/{}/web_static"
-             .format(name_split))
+        sudo("rm -rf {}}{}/web_static".format(data, name_split))
         sudo("rm -Rf /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/{} /data/web_static/current"
-             .format(name_split))
+        sudo("ln -s {}{} /data/web_static/current".format(data, name_split))
         return True
     except:
         return False
