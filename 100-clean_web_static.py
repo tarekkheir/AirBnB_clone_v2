6d01ce0bd588c3c44b1d953781066b8ceef3c9f2
Tarek <kheirtarek@hotmail.fr>
2021-04-20 08:16:47 +0200

    task 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -11,13 +11,15 @@ env.hosts = ['35.243.232.238', '35.237.104.223']
 
 def do_clean(number=0):
     """ delete files function"""
-    if int(number) == 0 or int(number) == 1:
-        number = 1
+    if int(number) <= 1:
+        number = 2
     else:
-        number = int(number)
+        number = int(number) + 1
 
-    local("ls -1tr /versions/* | tail -n +{} | xargs rm -Rf"
-          .format(number))
-    run("ls -1tr  /data/web_static/releases/* | tail -n +{}\
-          | xargs rm -Rf"
-          .format(number))
+    with lcd("versions"):
+        local("ls -1tr /versions/* | tail -n +{} | xargs -d rm -Rf"
+              .format(number))
+    with cd("/data/web_static/releases"):
+        run("ls -1tr  /data/web_static/releases/* | tail -n +{}\
+            | xargs -d rm -Rf"
+            .format(number))
