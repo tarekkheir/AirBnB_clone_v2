a773fe76ab9669d07d141ccdfbdfdeaf4a10d0c8
root <tarek.kheirtarek@hotmail.com>
2021-05-02 14:55:05 +0200

    task 1 pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 def hello():
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def HBNB_task1():
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
