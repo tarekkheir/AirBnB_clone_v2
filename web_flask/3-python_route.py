8184a9475d867f0c5c59c56f34cfaf59dcb5752f
root <tarek.kheirtarek@hotmail.com>
2021-05-02 15:12:54 +0200

    fix task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,7 +22,7 @@ def c_text(text):
 
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/(<text>)', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     return "Python {}".format(text.replace("_", " "))
 
